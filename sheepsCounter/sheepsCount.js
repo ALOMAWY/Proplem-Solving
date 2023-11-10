4136706c5c8a415aca73d4113759c50c4f408a4f
aldabbas <aldabbas333abdalrahman@gmail.com>
2023-11-11 02:09:12 +0300

    Add Sheeps Counter Files

---
+++ sheepsCounter/sheepsCount.js
@@ -0,0 +1,32 @@
+let mySheeps = [
+  true,
+  true,
+  true,
+  false,
+  true,
+  true,
+  true,
+  true,
+  true,
+  false,
+  true,
+  false,
+  true,
+  false,
+  false,
+  true,
+  true,
+  true,
+  true,
+  true,
+  false,
+  false,
+  true,
+  true,
+];
+
+function countSheep(sheep) {
+  return sheep.filter((e) => e == true).length;
+}
+
+console.log(countSheep(mySheeps));
