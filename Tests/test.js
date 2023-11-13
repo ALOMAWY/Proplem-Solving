a6d9838f7ba943ceb593273e8268aa61280ae361
aldabbas <aldabbas333abdalrahman@gmail.com>
2023-11-13 03:12:08 +0300

    Add New Files

--- Tests/test.js
+++ Tests/test.js
@@ -1,11 +1,21 @@
-const reverseSeq = (n) => {
-  console.log(Array(n));
-  return Array(n)
-    .fill(0)
-    .map((e, i) => n - i);
-};
-console.log(reverseSeq(4));
+// const reverseSeq = (n) => {
+//   console.log(Array(n));
+//   return Array(n)
+//     .fill(0)
+//     .map((e, i) => n - i);
+// };
+// console.log(reverseSeq(4));
 
-Array(4)
-  .fill(0)
-  .map((e, i) => 4 - i);
+// Array(4)
+//   .fill(0)
+//   .map((e, i) => 4 - i);
+
+function numbers_range(number) {
+  return Array(number + 1)
+    .fill(0)
+    .map((e, i) => number - i)
+    .reverse()
+    .join(" ");
+}
+
+console.log(numbers_range(6));
